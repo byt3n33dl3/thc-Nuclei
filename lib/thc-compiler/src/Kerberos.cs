8603bf55ed3d75244f08bd443f021c4b5923c138
pxcs <sulabaziz09@gmail.com>
2024-10-28 10:50:49 +0800

    System value (Kubernets 0x9)

---
+++ lib/thc-compiler/src/Kerberos.cs
@@ -0,0 +1,88 @@
+﻿using Rubeus;
+using SharpMapExec.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static Rubeus.Interop;
+
+namespace SharpMapExec.Lib
+{
+    public class kerberos
+    {
+        public static void Kerberos(string[] users, string domain, string[] passwords, string[] hashes, string ticket, KERB_ETYPE encType, string dc, string[] computernames, string module, string moduleargument, string path, string destination, List<string> flags, string protocol)
+        {
+            StartJob(users, domain, passwords, hashes, ticket, encType, dc, computernames, module, moduleargument, path, destination, flags, protocol);
+            //var listOfTasks = new List<Task>();
+            //listOfTasks.Add(new Task(() => StartJob(users, domain, passwords, hashes, ticket, encType, dc, computernames, module, moduleargument, path, destination, flags, protocol)));
+            //Tasks.StartAndWaitAllThrottled(listOfTasks, 1);
+        }
+
+        public static void StartJob(string[] users, string domain, string[] passwords, string[] hashes, string ticket, KERB_ETYPE encType, string dc, string[] computernames, string module, string moduleargument, string path, string destination, List<string> flags, string protocol)
+        {
+            AToken.MakeToken("Fake", "Fake", "Fake");
+            Console.WriteLine("------------------");
+            
+            if (String.IsNullOrEmpty(ticket))
+            {
+                var secrets = hashes.Length > 0 ? hashes : passwords;
+                foreach (string user in users)
+                {
+                    foreach (string secret in secrets)
+                    {
+                        string hash;
+                        if (passwords.Length > 0)
+                        {
+                            string salt = String.Format("{0}{1}", domain.ToUpper(), user);
+                            hash = Crypto.KerberosPasswordHash(encType, secret, salt);
+                        }
+                        else
+                        {
+                            hash = secret;
+                        }
+                        Console.WriteLine(string.Format("[*] User:   {0}", user));
+                        Console.WriteLine(string.Format("[*] Domain: {0}", domain));
+                        Console.WriteLine(string.Format("[*] Secret: {0}", secret));
+                        string ticketoutput = SecurityContext.AskTicket(user, domain, hash, encType, dc);
+                        if (ticketoutput.Contains("[+] Ticket successfully imported!"))
+                            Console.WriteLine("[+] Ticket successfully imported!");
+                        else
+                        {
+                            Console.WriteLine("[-] Could not request TGT");
+                            continue;
+                        }
+                        if (protocol.ToLower() == "smb")
+                            Scan.SMB(computernames, module);
+                        else if (protocol.ToLower() == "winrm")
+                            Scan.WINRM(computernames, module, moduleargument, path, destination, flags);
+                        else if (protocol.ToLower() == "reg32")
+                            Scan.REG32(computernames, module);
+                        else if (protocol.ToLower() == "ldap")
+                            Scan.LDAP(module, domain, dc);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine(string.Format("[*] Ticket: {0}", ticket));
+                string ticketoutput = SecurityContext.ImportTicket(ticket);
+                if (ticketoutput.Contains("[+] Ticket successfully imported!"))
+                    Console.WriteLine("[+] TGT imported successfully!");
+                else
+                {
+                    Console.WriteLine("[-] Could not import TGT");
+                    return;
+                }
+                if (protocol.ToLower() == "smb")
+                    Scan.SMB(computernames, module);
+                else if (protocol.ToLower() == "winrm")
+                    Scan.WINRM(computernames, module, moduleargument, path, destination, flags);
+                else if (protocol.ToLower() == "reg32")
+                    Scan.REG32(computernames, module);
+                else if (protocol.ToLower() == "ldap")
+                    Scan.LDAP(module, domain, dc);
+            }
+
+            AToken.RevertFromToken();
+        }
+    }
+}
---
+++ static/thc-compiler/src/Ntlm.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Management.Infrastructure;
+using SharpMapExec.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static SharpMapExec.Helpers.SecurityContext;
+
+namespace SharpMapExec.Lib
+{
+    public class ntlm
+    {
+        public static void Ntlm(string[] users, string domain, string[] passwords, string[] hashes, string[] computernames, string domainController, string module, string moduleargument,string path, string destination, List<string> flags, string protocol)
+        {
+            //StartJob(users, domain, passwords, hashes, computernames, module, moduleargument, path, destination, flags, protocol);
+            var listOfTasks = new List<Task>();
+            listOfTasks.Add(new Task(() => StartJob(users, domain, passwords, hashes, computernames, domainController, module, moduleargument, path, destination, flags, protocol)));
+            Tasks.StartAndWaitAllThrottled(listOfTasks, 1);
+        }
+
+        public static void StartJob(string[] users, string domain, string[] passwords, string[] hashes, string[] computernames, string domainController, string module, string moduleargument, string path, string destination, List<string> flags, string protocol)
+        {
+            var secrets = hashes != null ? hashes : passwords;
+ 
+            if (hashes != null)
+            {
+                foreach (string user in users)
+                {
+                    foreach (string password in secrets)
+                    {
+                        Console.WriteLine("------------------");
+                        Console.WriteLine(string.Format("[*] User:   {0}", user));
+                        Console.WriteLine(string.Format("[*] domain: {0}", domain));
+                        Console.WriteLine(string.Format("[*] secret: {0}", password));
+                        Console.WriteLine();
+                        SetThreadToken(user, domain, password);
+                        if (protocol.ToLower() == "smb")
+                        {
+                            Scan.SMB(computernames, module);
+                        }
+                        else if (protocol.ToLower() == "winrm")
+                        {
+                            Scan.WINRM(computernames, module, moduleargument, path, destination, flags);
+                        }
+                        else if (protocol.ToLower() == "reg32")
+                        {
+                            Scan.REG32(computernames, module);
+                        }
+                        else if (protocol.ToLower() == "domain")
+                        {
+                            Scan.LDAP(module, domain, domainController);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (string user in users)
+                {
+                    foreach (string password in secrets)
+                    {
+                        Console.WriteLine("------------------");
+                        Console.WriteLine(string.Format("[*] User:   {0}", user));
+                        Console.WriteLine(string.Format("[*] domain: {0}", domain));
+                        Console.WriteLine(string.Format("[*] secret: {0}", password));
+                        Console.WriteLine();
+                        using (new Impersonator.Impersonation(domain, user, password))
+                        {
+                            if (protocol.ToLower() == "smb")
+                            {
+                                Scan.SMB(computernames, module);
+                            }
+                            else if (protocol.ToLower() == "winrm")
+                            {
+                                Scan.WINRM(computernames, module, moduleargument, path, destination, flags);
+                            }
+                            else if (protocol.ToLower() == "cim")
+                            {
+                                foreach (string computername in computernames)
+                                {
+                                    CimSession cimSession;
+                                    cimSession = Cim.newSession(computername, domain, user, password, flags.Contains("impersonate"));
+                                    Scan.CIM(cimSession, module);
+                                }
+                            }
+                            else if (protocol.ToLower() == "reg32")
+                            {
+                                Scan.REG32(computernames, module);
+                            }
+                            else if (protocol.ToLower() == "domain")
+                            {
+                                Scan.LDAP(module, domain, domainController);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
